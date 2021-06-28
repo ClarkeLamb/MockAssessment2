cf68ebc23c4e081d954bc729b1f35efb182783ce
clarke <cf.lamb25@gmail.com>
2021-06-28 17:13:58 -0400

    First commit for mock assessment2

---
+++ MockAssessment2a/Challenge.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssessment2a
+{
+    class Challenge
+    {
+        public string Character { get; set; }
+        public int AttackValue { get; set; }
+        public string Planets { get; set; }
+
+        public Challenge()
+        {
+
+        }
+        public Challenge(string character, int attackValue)
+        {
+            Character = character;
+            AttackValue = attackValue;
+        }
+
+        public static string AddStarWarsCharacters(string userInput)
+        {
+            if(userInput == "Yoda")
+            {
+                return "Yoda";
+            }
+            return "";
+        } 
+
+        public void DeathStarCombat(Dictionary<string, int>deathStar)
+        {
+            foreach(KeyValuePair<string, int>characterInfo in deathStar)
+            {
+                if (characterInfo.Value.Equals(99))
+                {
+                    Console.WriteLine($"{characterInfo.Key}, is the Jedi with highest attack value.");
+                }
+            }
+        }
+
+        public void ConvertPlanets(string[] planets)
+        {
+            List<string> planetList = planets.Reverse().ToList();
+            foreach (var i in planetList)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+
+        public void AverageDroids(List<double> droid)
+        {
+            double average = 0;
+            List<double> evenDroids = new List<double>();
+            evenDroids.Add(12);
+            evenDroids.Add(212);
+
+            foreach (double evenBot in evenDroids)
+            {
+                average = evenDroids.Average();
+                
+            }
+            Console.WriteLine(average);
+
+           
+        }
+
+        public static void TryToCatchDarthVader(string userInputTwo)
+        {
+            try
+            {
+                int number = int.Parse(userInputTwo);
+
+                Console.WriteLine("Vader was captured!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Vader got awat!");
+
+            }
+        }
+
+        //public static string TryToCatchDarthVader(string userInputTwo)
+        //{
+        //    try
+        //    {
+        //        int answer = int.Parse(userInputTwo);
+        //        return "Vader was Captured!";
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        return $"(Error: {ex})";
+
+        //    }
+
+
+
+
+    }
+}
---
+++ MockAssessment2a/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockAssessment2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var yoda = new Challenge("Yoda", 99);
+            //var kenobi = new Challenge("Obi-Wan Kenobi", 82);
+            //var lukeSkywalker = new Challenge("Luke Skywalker", 88);
+            //var anakin = new Challenge("Anakin Skywalker", 97);
+            //var windu = new Challenge("Mace Windu", 80);
+
+            //#2
+            Dictionary<string, int> deathStar = new Dictionary<string, int>();
+            deathStar.Add("Yoda", 99);
+            deathStar.Add("Obi-Wan Kenobi", 82);
+            deathStar.Add("Luke Skywalker", 88);
+            deathStar.Add("Anakin Skywalker", 97);
+            deathStar.Add("Mace Windu", 80);
+
+            var stepTwo = new Challenge();
+            stepTwo.DeathStarCombat(deathStar);
+
+            //#3
+            string[] planets = { "Naboo", "Tatooine", "Hoth", "Mustafar" };
+
+            var stepThree = new Challenge();
+            stepThree.ConvertPlanets(planets);
+
+            //#4
+            List<double> droid = new List<double>();
+            droid.Add(12);
+            droid.Add(67);
+            droid.Add(212);
+            droid.Add(55);
+
+            var stepFour = new Challenge();
+            stepFour.AverageDroids(droid);
+
+            //#5
+            Console.WriteLine("Was 1,2,3,4,5 or 6 your favorite star wars movie?");
+            string userInputTwo = Console.ReadLine();
+            TryToCatchDarthVader(userInputTwo);
+
+
+        }
+
+        //public static void TryToCatchDarthVader(string userInputTwo)
+        //{
+        //    try
+        //    {
+        //        int number = int.Parse(userInputTwo);
+
+        //        Console.WriteLine("Vader was captured!");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Vader got awat!");
+
+        //    }
+        //}
+
+    }
+}
